35bb7e12fe9b283e8a72783ee1d6a613a1e4cd91
Jeferson <jefer.hernandez1@gmail.com>
2025-07-30 12:27:36 -0500

    Continuacion de sesion con OFAC

--- 09-laravel/resources/views/auth/register.blade.php
+++ 09-laravel/resources/views/auth/register.blade.php
@@ -1,56 +1,3 @@
-{{-- <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
-
 @extends('layouts.app')   {{-- layouts nombre de carpeta y luego llama al app.blade.php --}}
 @section('title', 'Register')
 @section('content')
@@ -58,7 +5,7 @@
 
     {{-- Contenido principal --}}
     <main class="bg-[url(images/fondoPetsApp.png)] bg-cover w-full min-h-[100dvh] flex flex-col justify-center items-center">
-        <div class="bg-[#0006] w-96 text-white p-10 rounded-lg flex flex-col justify-center items-center">
+        <div class="bg-[#0006] w-96 text-white p-10 mt-20 mb-10 rounded-lg flex flex-col justify-center items-center">
             <h1 class="text-4xl flex gap-2 items-center pb-2 border-b-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
@@ -74,29 +21,55 @@
                     @endforeach
                 </div>
             @endif
-            <form method="POST" action="{{ route('login') }}" class="flex flex-col gap-6">
+            <form method="POST" action="{{ route('register') }}" class="flex flex-col gap-6">
                 @csrf
                 <div class="mt-4">
-                    <label class="mt-4" for="">Email:</label>
-                    <input type="text" name="email" placeholder="jonhw@gmail.com" class="input bg-[transparent] border-white" />
+                    <label class="mt-4" for="">Document:</label>
+                    <input type="text" name="document" placeholder="75678000" class="input bg-[transparent] border-white" value="{{ old('document') }}" />
                 </div>
                 <div>
-                    <label class="mt-4" for="">Password:</label>
+                    <label class="mt-4" for="">Full Name:</label>
+                    <input type="text" name="fullname" placeholder="Jhon Wick" class="input bg-[transparent] border-white" value="{{ old('fullname') }}"/>
+                </div>
+                <div>
+                    <label class="mt-4" for="">Gender:</label>
+                    <select name="gender" class="select bg-[transparent] border-white">
+                        <option value="">Select Gender...</option>
+                        <option value="Female" @if (old('gender') == 'Female' ) selected @endif>Female</option>
+                        <option value="Male" @if (old('gender') == 'Male' ) selected @endif>Male</option>
+                    </select>
+                </div>
+                <div>
+                    <label class="mt-4" for="">Birthdate:</label>
+                    <input type="date" name="birthdate" placeholder="2000-10-08" class="input bg-[transparent] border-white" value="{{ old('birthdate') }}"/>
+                </div>
+                <div>
+                    <label class="mt-4" for="">Phone Number:</label>
+                    <input type="text" name="phone" placeholder="3147895678" class="input bg-[transparent] border-white" value="{{ old('phone') }}"/>
+                </div>
+                <div>
+                    <label class="mt-4" for="">Email:</label>
+                    <input type="text" name="email" placeholder="jonhw@gmail.com" class="input bg-[transparent] border-white" value="{{ old('email') }}"/>
+                </div>
+                <div>
+                    <label class="mt-4" for="">Password Confirmation:</label>
                     <input type="password" name="password" placeholder="admin" class="input bg-[transparent] border-white" />  
                 </div>
                 <div>
+                    <label class="mt-4" for="">Password:</label>
+                    <input type="password" name="password_confirmation" placeholder="admin" class="input bg-[transparent] border-white"/>  
+                </div>
+                <div>
                     <button class="btn btn-outline w-full">
-                        Login
+                        Register
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                         </svg>
 
                     </button>
-                     @if (Route::has('password.request'))
-                        <a class="pb-1 border-b-2 mt-6 inline-block text-sm text-gray-200" href="{{ route('password.request') }}">
-                            Forgot your password?
-                        </a>
-                    @endif
+                    <a class="pb-1 border-b-2 mt-6 inline-block text-sm text-gray-200" href="{{ route('login') }}">
+                        Already registered?
+                    </a>
                 </div>
             </form>
         </div>
